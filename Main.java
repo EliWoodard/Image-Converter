679576adccc3124b25d09220eacd2d750011df3d
Elijah <eliwoodard@u.boisestate.edu>
2023-03-28 22:12:49 -0600

    Added Javadocs

--- Main.java
+++ Main.java
@@ -7,8 +7,30 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+/**
+ * The Image Converter program converts images to black and white, either for a
+ * single image or all images in a specified folder. Progress can be displayed
+ * using a progress bar. The program can also delete all files in the input and
+ * output folders using the -delete command line argument.
+ * 
+ * @version 1.0
+ * @author Eli Woodard
+ */
+
 public class Main {
-    
+
+    /**
+     * The main method of the Image Converter program. Converts all images in a
+     * specified input folder to black and white,
+     * and saves them in a specified output folder. Progress can be displayed using
+     * a progress bar. The program can also delete
+     * all files in the input and output folders using the -delete command line
+     * argument.
+     *
+     * @param args Command line arguments. The program accepts the -delete argument
+     *             to delete all files in the input and output
+     *             folders.
+     */
     public static void main(String[] args) {
         String inputPath = "Convert-Images"; // Path to folder containing images to convert
         String outputPath = "Convert-Images-BW"; // Path to folder to save converted images
@@ -19,15 +41,27 @@ public class Main {
             deleteDirectory(new File(outputPath));
             System.out.println("All files deleted.");
         } else {
-            convertImagesToBW(inputPath, outputPath, outputFormat, showProgressBar); // Convert images in folder to black and white
+            convertImagesToBW(inputPath, outputPath, outputFormat, showProgressBar); // Convert images in folder to
+                                                                                     // black and white
             // String inputFilePath = "Convert-Image/input.jpg"; // Path to image to convert
-            // String outputFilePath = "Convert-Image/output.jpg"; // Path to save converted image
-            // convertImageToBW(inputFilePath, outputFilePath, outputFormat); // Convert single image to black and white
+            // String outputFilePath = "Convert-Image/output.jpg"; // Path to save converted
+            // image
+            // convertImageToBW(inputFilePath, outputFilePath, outputFormat); // Convert
+            // single image to black and white
         }
     }
-    
-    // Convert all images in a folder to black and white
-    public static void convertImagesToBW(String inputPath, String outputPath, String outputFormat, boolean showProgressBar) {
+
+    /**
+     * Convert all images in a folder to black and white.
+     *
+     * @param inputPath       the path to the input folder containing images to
+     *                        convert
+     * @param outputPath      the path to the output folder to save converted images
+     * @param outputFormat    the output format of the converted images
+     * @param showProgressBar whether to display a progress bar during conversion
+     */
+    public static void convertImagesToBW(String inputPath, String outputPath, String outputFormat,
+            boolean showProgressBar) {
         File inputFolder = new File(inputPath);
         File[] files = inputFolder.listFiles();
         int numFiles = files.length;
@@ -46,7 +80,8 @@ public class Main {
         }
         for (File file : files) {
             if (file.isFile()) {
-                String outputFilePath = outputPath + "/" + getFileNameWithoutExtension(file.getName()) + "." + outputFormat;
+                String outputFilePath = outputPath + "/" + getFileNameWithoutExtension(file.getName()) + "."
+                        + outputFormat;
                 convertImageToBW(file.getPath(), outputFilePath, outputFormat);
                 numConverted++;
                 if (showProgressBar && progressBar != null) {
@@ -60,9 +95,13 @@ public class Main {
         }
     }
 
-
-    
-    // Convert a single image to black and white
+    /**
+     * Convert a single image to black and white.
+     *
+     * @param inputFilePath  the path to the input image to convert
+     * @param outputFilePath the path to the output image to save
+     * @param outputFormat   the output format of the converted image
+     */
     public static void convertImageToBW(String inputFilePath, String outputFilePath, String outputFormat) {
         try {
             File inputFile = new File(inputFilePath);
@@ -75,7 +114,8 @@ public class Main {
                 System.out.println("Unsupported image format: " + inputFilePath);
                 return;
             }
-            BufferedImage bwImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            BufferedImage bwImage = new BufferedImage(image.getWidth(), image.getHeight(),
+                    BufferedImage.TYPE_BYTE_GRAY);
             Graphics2D graphics = bwImage.createGraphics();
             graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             graphics.drawImage(image, 0, 0, null);
@@ -83,12 +123,17 @@ public class Main {
             ImageIO.write(bwImage, outputFormat, new File(outputFilePath));
             System.out.println("Image converted to black and white: " + outputFilePath);
         } catch (IOException e) {
-        System.out.println("Error converting image to black and white: " + inputFilePath);
-        e.printStackTrace();
+            System.out.println("Error converting image to black and white: " + inputFilePath);
+            e.printStackTrace();
         }
     }
-    
-    // Get the file name without the extension
+
+    /**
+     * Returns the name of the file without its extension.
+     * 
+     * @param fileName the name of the file
+     * @return the name of the file without its extension
+     */
     public static String getFileNameWithoutExtension(String fileName) {
         int dotIndex = fileName.lastIndexOf(".");
         if (dotIndex == -1) {
@@ -98,7 +143,11 @@ public class Main {
         }
     }
 
-    // Delete all files in a directory
+    /**
+     * Deletes all files in a directory.
+     *
+     * @param directory the directory to delete
+     */
     public static void deleteDirectory(File directory) {
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
@@ -114,4 +163,3 @@ public class Main {
         }
     }
 }
-
